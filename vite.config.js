2d99d18922feee3711bbe7542b89335c83e95690
manickamSiva <manickamsiva617@gmail.com>
2026-03-08 15:11:59 +0530

    Add GitHub Pages deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/jewellery-site/',
   plugins: [react()],
   server: {
     proxy: {
